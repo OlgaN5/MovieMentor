2ad3fa3acb97949ddaba7b70335abf9c3afb6d37
Olya <naumchik.olya55@gmail.com>
2023-10-01 21:55:42 +0300

    last last version

--- config/database.js
+++ config/database.js
@@ -1,6 +1,13 @@
 const Sequelize = require('sequelize')
 
-module.exports = new Sequelize(process.env.DATABASE, process.env.USER_DB, process.env.USER_PASSWORD, {
-    host: process.env.HOST,
-    dialect: process.env.DIALECT
+module.exports = new Sequelize('psagflzi', 'psagflzi', 'IQeihzw6C63v_WrKH3qXJnwA4XxelqSg', {
+    host: 'surus.db.elephantsql.com',
+    dialect: 'postgres'
 })
+
+// const Sequelize = require('sequelize')
+
+// module.exports = new Sequelize(process.env.DATABASE, process.env.USER_DB, process.env.USER_PASSWORD, {
+//     host: process.env.HOST,
+//     dialect: process.env.DIALECT
+// })

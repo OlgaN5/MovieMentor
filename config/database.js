6c37dbba9461aa41c1f0c467339844a6b8173e3f
Olya <naumchik.olya55@gmail.com>
2023-10-07 19:59:55 +0300

    versel

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-module.exports = new Sequelize('verceldb', 'default', 'O1BxWcU0LHXv', {
+module.exports = new Sequelize('verceldb', 'default', 'TRCz0QXj9btD', {
     host: 'ep-cold-flower-38311113-pooler.us-east-1.postgres.vercel-storage.com',
     dialect: 'postgres',
     dialectOptions: {
